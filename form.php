c9df0236139e93a7bf055d02f55ea17ea7356a07
Prince Kumar Singh <princekumar2000.pks@gmail.com>
2022-07-02 00:42:44 +0530

    v9mail

--- form.php
+++ form.php
@@ -9,19 +9,25 @@ $subject = $_POST['subject'];
 $message = $_POST['inputMessage'];
 
 $to = "scale@scaleservice.in";
+$to_user = $email;
 $subject = `$subject` . " | Scale ";
 
-// The following text will be sent
-// Name = user entered name
-// Email = user entered email
-// Message = user entered message
 $txt = "Name = " . $name . "\r\n Subject = " . $subject . "\r\n Email = "
     .  $email . "\r\n Message =" . $message;
+$user_welcome_msg = `Hey there {$name},
+Welcome to SCALE. Thank you for contacting us. You will soon get a reply from the team. We are the best solution for your problem.
+Thank You,
+Team SCALE.`;
 
 $headers = "From: Scale-Services@scaleservice.in" . "\r\n" .
-    "BCC: me@divelink.in";;
+    "BCC: me@divelink.in";
+$headers2 = "From:Team-Scale@scaleservices.in" . "\r\n" .
+    "BCC: me@divelink.in";
 if ($email != NULL) {
     mail($to, $subject, $txt, $headers);
+}
+if ($to_user != null) {
+    mail($to_user, "Team SCALE", $user_welcome_msg, $headers2);
 }
 
 // Redirect to
