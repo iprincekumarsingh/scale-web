1389f77926d5ff8f450c40a3d732b75a8aa70d4e
Prince Kumar Singh <princekumar2000.pks@gmail.com>
2022-07-02 00:14:08 +0530

    b5

--- form.php
+++ form.php
@@ -5,20 +5,21 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $subject = $_POST['subject'];
 
+
 $message = $_POST['inputMessage'];
 
 $to = "";
-$subject = `$subject`. " | Scale ";
+$subject = `$subject` . " | Scale ";
 
 // The following text will be sent
 // Name = user entered name
 // Email = user entered email
 // Message = user entered message
-$txt = "Name = " . $name . "\r\n Email = "
-    . $email . "\r\n Message =" . $message;
+$txt = "Name = " . $name . "\r\n Subject = " . $subject . "\r\n Email = "
+    .  $email . "\r\n Message =" . $message;
 
-$headers = "From: scale@scaleservice." . "\r\n".
-"BCC: me@divelink.in";;
+$headers = "From: scale@scaleservice." . "\r\n" .
+    "BCC: me@divelink.in";;
 if ($email != NULL) {
     mail($to, $subject, $txt, $headers);
 }
